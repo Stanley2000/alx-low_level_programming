55b9c8da86d0772a6d10673834a49dd5bf0693ca
stanley agbo <stanleykelechi2000@gmail.com>
2023-07-13 22:18:24 +0100

    added printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - The entry point of the c program
+ *Return: it indicates successful termination of the program
+ */
+
+ int main(void)
+ {
+ 	printf("with proper grammar, but the outcome is a pieace of art,\n");
+	return (0);
+ }
