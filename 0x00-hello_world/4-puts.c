7fddb70b7f400220194794129519e5ab31e2f921
stanley agbo <stanleykelechi2000@gmail.com>
2023-07-13 22:03:28 +0100

    update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+/**
+ * main - The entry point in the c program
+ *return - indicates successful termination of program
+ */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
