23ddb0a3c8bef3e0f60296c3bf031c92537cd6bd
stanley agbo <stanleykelechi2000@gmail.com>
2023-07-13 23:27:40 +0100

    update

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,8 +5,8 @@
  *Return: it indicates successful termination of the program
  */
 
- int main(void)
- {	
- 	printf("with proper grammar, but the outcome is a piece of art,\n");
+int main(void)
+{	
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
- }
+}
