e068557407e6e73e6b4f7ea1d52fc2c1d74c6945
stanley agbo <stanleykelechi2000@gmail.com>
2023-07-13 23:47:19 +0100

    update

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,11 +2,11 @@
 #include <unistd.h>
 
 /**
- *	 main - The entry of a c program.
- *	Return: Always 0 (Success)
+ * main - entry point of the c program
+ * Return: Aways 0
  */
-int main(void)
+ int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" -Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
