dcdfd532b48285e6504f5dd8316bd3a5da382a14
stanley agbo <stanleykelechi2000@gmail.com>
2023-07-14 21:54:42 +0100

    added 3-print_alphabets.c file

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+  * main - The entry of the c program
+  * Return: Shows successful termination of the program
+  */
+
+int main(void)
+{
+	char start = 'a';
+	char end = 'z';
+	char End = 'z';
+	cher Start = 'a';
+
+	for (; start <= end; start++)
+	{
+		putchar(start);
+	}
+
+	for (; Start <= End; Start++)
+	{
+		putchar(Start);
+	}
+	putchar('\n');
+
+	return (0);
+}
