c7e540e8fbdc1427a6994aac7c0a4e3ea27afe23
stanley agbo <stanleykelechi2000@gmail.com>
2023-07-14 22:18:40 +0100

    added 5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+  * main - The entry point of the program
+  * Return: Shows the successful termination of the program
+  */
+
+int main(void)
+{
+	int base = 0;
+
+	for (; base < 10; base++)
+	{
+		printf("%d", base);
+	}
+	putchar('\n');
+}
