195894d3917abd6978e0cf853ba79a0846848c1b
stanley agbo <stanleykelechi2000@gmail.com>
2023-07-14 22:08:17 +0100

    added 4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+  * main - The entry of the c program
+  * Return: Shows successful termination of the program
+  */
+
+int main(void)
+{
+	char start = 'a';
+	char end = 'z';
+
+	for (; start <= end; start++)
+	{
+		if (!(start == 'q' || start == 'e'))
+		{
+			putchar(start);
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
