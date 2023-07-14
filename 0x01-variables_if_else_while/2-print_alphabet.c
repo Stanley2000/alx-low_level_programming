c42311c77f815b2004406034207e92efdedf3c80
stanley agbo <stanleykelechi2000@gmail.com>
2023-07-14 21:38:11 +0100

    added 2-print_alphabet.c file

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+  * main - The entry of the c program
+  * Return: Shows successful termination of the program
+  */
+
+int main(void)
+{
+	char start = 'a';
+	char end = 'z';
+
+	for (; start <= end; start++)
+	{
+		putchar(start);
+	}
+	putchar('\n\);
+	return (0);
+}
