0f8c51b364b2b48ab1eeac57c570d62bc0dcde8d
stanley agbo <stanleykelechi2000@gmail.com>
2023-07-19 00:20:51 +0100

    added a new file

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+iinclude "main.h"
+
+/**
+  * print_last_digit - prints out the last digit of an integer
+  * @n: The number to return its last digit
+  * Return: shows successful termination of the function
+  */
+
+int print_last_digit(int n)
+{
+	int a;
+
+	a = n % 10;
+	return (a);
+
+}
