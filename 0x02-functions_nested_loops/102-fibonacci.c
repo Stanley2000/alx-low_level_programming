374868cd98f2659d91e3ddded6b429689479fd3c
stanley agbo <stanleykelechi2000@gmail.com>
2023-07-20 00:15:12 +0100

    added a new file

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - prints the first 50 Fibonacci numbers
+ * Return: returns 0 (success)
+ */
+int main(void)
+{	long int a, b, next;
+
+	a = 1;
+	b = 2;
+	for (int c = 1; c <= 50; ++c)
+	{
+		if (c < 50)
+		{
+			printf("%ld, ", a);
+		}
+		else
+		{
+			printf("%ld\n", a);
+		}
+		next = a + b;
+		a = b;
+		b = next;
+		}
+		return (0);
+}
