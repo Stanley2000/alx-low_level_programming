654ccd0fe0017103f057d35369a4ce67e8595ff3
stanley agbo <stanleykelechi2000@gmail.com>
2023-07-19 13:11:09 +0100

    updated

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,26 +9,32 @@
   */
 void print_to_98(int n)
 {
-	for (; n <= 98; n++)
+	if (n <= 98)
 	{
-		if (n < 98)
+		for (; n <= 98; n++)
 		{
-			printf("%d, ", n);
-		}
-		else if (n == 98)
-		{
-			printf("%d\n", n);
+			if (n < 98)
+			{
+				printf("%d, ", n);
+			}
+			else if (n == 98)
+			{
+				printf("%d\n", n);
+			}
 		}
 	}
-	for (; n >= 98; n--)
+	else if (n >= 98)
 	{
-		if (n > 98)
+		for (; n >= 98; n--)
 		{
-			printf("%d, ", n);
-		}
-		else if (n == 98)
-		{
-			printf("%d\n", n);
+			if (n > 98)
+			{
+				printf("%d, ", n);
+			}
+			else if (n == 98)
+			{
+				printf("%d\n", n);
+			}
 		}
 	}
 }
