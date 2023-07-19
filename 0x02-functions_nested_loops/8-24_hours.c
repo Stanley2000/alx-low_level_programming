7a8232bde203d202feaffe9e3f5df06a8bc0c95c
stanley agbo <stanleykelechi2000@gmail.com>
2023-07-19 09:17:09 +0100

    added a jack_bauer file

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,23 @@
+#include "main"
+
+/**
+  * jack_bauer - a function that print out minutes of the day of jack Bauer
+  * void: shows the function returns nothing
+  */
+void jack_bauer(void)
+{
+	int a, b;
+
+	for (a = 0; a <= 23; a++)
+	{
+		for (b = 0; b <= 59; b++)
+		{
+			_putchar(a / 10 + '0');
+			_putchar(a % 10 + '0');
+			_putchar(':');
+			_putchar(b / 10);
+			_putchar(b % 10);
+			_putchar('\n');
+		}
+	}
+}
