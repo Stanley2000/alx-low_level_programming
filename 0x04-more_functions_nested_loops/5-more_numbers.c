fafe0b1d98a5d001c95de2d01da282f35d3137c0
stanley agbo <stanleykelechi2000@gmail.com>
2023-07-21 00:10:31 +0100

    added

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * more_numbers - print numbers from 0 t0 14, 10 times
+ */
+
+void more_numbers(void)
+{
+	int a;
+	int b;
+
+	for (a = 0; a < 10; i++)
+	{
+		for (b = 0; b < 15; b++)
+		{
+			if (b >= 10)
+				_putchar(b / 10 + '0');
+			_putchar(b  + '0');
+		}
+		_putchar('\n');
+	}
+}
