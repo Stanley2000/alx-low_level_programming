f8d8b0af6f13ad8bfa601692927e22b8c272ce87
stanley agbo <stanleykelechi2000@gmail.com>
2023-07-20 22:12:00 +0100

    added a new file

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main"
+
+/** _isupper - a function that checks for uppercase character
+  * @cc: character to be checked
+  * Return: shows successful termination of the function
+  */
+
+int _isuppper(int c)
+{
+
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+
+}
