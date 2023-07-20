0c6f0373dcb6ca7ec7ca2ccf115467a6b3d756df
stanley agbo <stanleykelechi2000@gmail.com>
2023-07-20 23:25:09 +0100

    added new file

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+  * print_most_numbers - print numbers
+  */
+
+void print_most_numbers(void)
+{
+	int a = 0;
+
+	for (a; a < 10; a++)
+	{
+		if (a != 2 || a != 4)
+		{
+			_putchar(a + '0');
+		}
+
+	}
+	_putchar('\n');
+}
