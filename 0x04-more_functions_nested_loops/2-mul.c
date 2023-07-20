6f9c724c389df5f1d472b2453a2fa50f2d47a695
stanley agbo <stanleykelechi2000@gmail.com>
2023-07-20 22:42:06 +0100

    added 2-mul.c file

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+  * mul - multiples two integers
+  * @a: parameter 1
+  * @b: parameter 2
+  * Return: shows successful termination of the function
+  */
+int mul(int a, int b)
+{
+	return (a * b);
+}
