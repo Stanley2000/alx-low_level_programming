52adb37684a12095cf939affc009f7c54c7988d2
stanley agbo <stanleykelechi2000@gmail.com>
2023-07-20 22:18:23 +0100

    updated

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,11 +1,12 @@
 #include "main"
 
-/** _isupper - a function that checks for uppercase character
-  * @cc: character to be checked
+/**
+  * _isupper - a function that checks for uppercase character
+  * @c: character to be checked
   * Return: shows successful termination of the function
   */
 
-int _isuppper(int c)
+int _isupper(int c)
 {
 
 	if (c >= 65 && c <= 90)
