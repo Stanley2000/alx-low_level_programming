a06ed3129eb31aff65fb69ce313a2500f58fcd0c
stanley agbo <stanleykelechi2000@gmail.com>
2023-07-24 22:30:54 +0100

    update

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,11 +9,11 @@
 void print_rev(char *s)
 {
 	int len = strlen(s);
-	int i = len - 1;
+	int i;
 
-	for (i; i >= 0; i--)
+	for (i= len - 1; i >= 0; i--)
 	{
-		putchar(s[i]);
+		_putchar(s[i]);
 	}
-	putchar('\n');
+	_putchar('\n');
 }
