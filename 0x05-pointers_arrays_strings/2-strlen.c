4679f15fb0b8cdf6c315be30d852b5fe0cba7f3f
stanley agbo <stanleykelechi2000@gmail.com>
2023-07-24 21:13:54 +0100

    added a new file

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+include "main.h"
+
+/**
+  * _strlen - a function that count the lenght of a string
+  * @s: pointer argument
+  */
+
+int _strlen(char *s)
+{
+	int a;
+	a = strlen(s);
+	return (a);
+}
