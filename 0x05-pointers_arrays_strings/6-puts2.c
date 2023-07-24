d357ff6fcc5c740fb82cc309a28243046a2c97fa
stanley agbo <stanleykelechi2000@gmail.com>
2023-07-24 23:42:05 +0100

    file

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+  * puts2 - a function
+  * @str: function pointer parameter
+  */
+
+void puts2(char *str)
+{
+	int i, j;
+
+	j = strlen(s);
+	for (i = 0; i < j; i++)
+	{
+		_putchar(s[i]);
+		_putchar('\n');
+	}
+
+}
