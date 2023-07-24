94a447236ac3d8c777dceca52d77e7b5e4068e5e
stanley agbo <stanleykelechi2000@gmail.com>
2023-07-24 21:19:05 +0100

    update

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -4,11 +4,13 @@
 /**
   * _strlen - a function that count the lenght of a string
   * @s: pointer argument
+  * Return: shows successful termination of the function
   */
 
 int _strlen(char *s)
 {
 	int a;
+
 	a = strlen(s);
 	return (a);
 }
