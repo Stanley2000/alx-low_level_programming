1d58aa6f2c1cd6c772a1875be980b9229fbbce73
stanley agbo <stanleykelechi2000@gmail.com>
2023-07-24 23:30:51 +0100

    file

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <string.h>
+
+/**
+  * rev_string - function that reverse a string
+  * @s: pointer argument
+  */
+
+void rev_string(char *s)
+{
+	int len = strlen(s)
+	int i, j;
+
+	for (i = 0, j = len - 1; i < j; i++, j--)
+	{
+		char temp = s[i];
+		s[i] = s[j];
+		s[j] = temp;
+	}
+}
