84ace5b837028b367aa5ccc0cf33b7a062855621
stanley agbo <stanleykelechi2000@gmail.com>
2023-07-27 10:30:46 +0100

    new file

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,27 @@
+#include <string.h>
+#include "main.h"
+
+/**
+  * _strcat -  a function that join two strings
+  * @dest: the pointer to the source string
+  * @src: the pointer to the destination
+  * Return: a pointer to the resulting string
+  */
+char *_strcat(char *dest, char *src)
+{
+	size_t i, j;
+
+	i = 0;
+	j = 0;
+
+	while (dest[i] != '\0')
+		i++;
+
+	while (src[j] != '\0')
+	{
+		dest[i + j] = src[j];
+		j++
+	}
+	dest[i + j] = '\0';
+	return (dest);
+}
