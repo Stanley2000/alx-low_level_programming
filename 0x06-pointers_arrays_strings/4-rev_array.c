8286cb7ee33dca96280f43c2e7947a860c765012
stanley agbo <stanleykelechi2000@gmail.com>
2023-07-28 00:57:31 +0100

    new file

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+  * reverse_array - a function that reverse an array
+  * @a: the array
+  * @n: number of elements in the array
+  */
+
+void reverse_array(int *a, int n)
+{
+	int i, j, b;
+
+	i = 0;
+	j = n - 1;
+
+	while (i < n)
+	{
+		b = a[i];
+		a[i] = a[j];
+		a[j] = b;
+	}
+}
