c4529485804004dc0a8b84a6256ca2ddd50e006d
stanley agbo <stanleykelechi2000@gmail.com>
2023-07-28 01:29:54 +0100

    update

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -18,6 +18,7 @@ void reverse_array(int *a, int n)
 		b = a[i];
 		a[i] = a[j];
 		a[j] = b;
+
 		i++;
 	}
 }
---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * string_toupper -a function to  changes all lowercase letter
+ * of a string to uppercase
+ * @str: string to modify
+ * Return: the resulting string
+ */
+
+char *string_toupper(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+			str[i] = s[i] - 32;
+	}
+
+	return (str);
+}
