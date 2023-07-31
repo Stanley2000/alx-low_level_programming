7487d587c922a0a9ecef2092dba57b2bbd922466
stanley agbo <stanleykelechi2000@gmail.com>
2023-07-31 23:27:09 +0100

    new file

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+  * _strchr - a function that locate a character in a string
+  * @s: the string
+  * @c: the character
+  * Return: returns a pointer
+  */
+
+char *_strchr(char *s, char c)
+{
+	while (*S != '\0')
+	{
+		if (*S == c)
+			return (s);
+
+	}
+	s++;
+
+	return (NULL);
+}
