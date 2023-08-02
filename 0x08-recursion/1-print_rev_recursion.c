bc40f696dcd27d8c86fb7fbd4565b5acd4a6af41
stanley agbo <stanleykelechi2000@gmail.com>
2023-08-02 23:14:44 +0100

    new file

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+  * _print_rev_recursion - prints a string in reverse other
+  * @s: pointer argument to the string
+  */
+
+
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		_print_rev_recursion(++s);
+		_putchar(*s);
+	}
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,5 +1,6 @@
 #include <stdio.h>
 int _pow_recursion(int x, int y);
+void _print_rev_recursion(char *s);
 int factorial(int n);
 int _strlen_recursion(char *S);
 void _puts_recursion(char *s);
