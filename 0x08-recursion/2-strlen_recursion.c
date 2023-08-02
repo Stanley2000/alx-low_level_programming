b35817750ef5bf7175b961998ef228518874af28
stanley agbo <stanleykelechi2000@gmail.com>
2023-08-02 21:36:58 +0100

    new file

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
-  * _strle_recursion - a function that returns the length of a string
-  * @s: pointer to the string 
+  * _strlen_recursion - a function that returns the length of a string
+  * @s: pointer to the string
   *Return: returns the length of the string
   */
 
