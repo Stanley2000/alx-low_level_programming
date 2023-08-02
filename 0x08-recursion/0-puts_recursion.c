a20c2b16ff3b99f93a6557453e58ccde6a17895a
stanley agbo <stanleykelechi2000@gmail.com>
2023-08-02 20:29:06 +0100

    new files

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+  * _put_recursion - a prints out a string followed by a new line
+  * @s: a pointer argument to the string to be printed
+  */
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\0');
+		_putchar('\n');
+
+	}
+	else
+	{
+		_putchar(*S);
+		_puts_recursion(++s);
+	}
+}
