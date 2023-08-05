d054ad57b5d347bb4442caf13517e11abba7cbde
stanley agbo <stanleykelechi2000@gmail.com>
2023-08-05 02:34:23 +0100

    new file

---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+  * _islower - checks if a character is lower case character
+  * @c: the character to check
+  * Return: 1 if c is lowercase character, zero otherwise
+  */
+
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
