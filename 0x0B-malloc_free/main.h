f93f478afe556f5ac9f257f0e7e12e344d394310
stanley agbo <stanleykelechi2000@gmail.com>
2023-08-08 22:22:51 +0100

    file update

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+char *_strdup(char *str);
 char *create_array(unsigned int size, char c);
 char *str_concat(char *s1, char *s2);
 int **allow_grid(int width, int height);
