67a14272f2f11182a7987555e59d2f64061f8d24
stanley agbo <stanleykelechi2000@gmail.com>
2023-08-10 13:53:27 +0100

    new file

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,44 @@
+iinclude "main.h"
+/**
+  * string_nconcat - a function that joins two strings
+  * @s1: The destination string
+  * @s2: The source string
+  * @n: the size of destination to be added
+  * Return: returns the pointer to the allocated memory
+  */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	int i = 0, j = 0, k;
+	char *p;
+
+	while (s1[i] != '\0')
+		i++;
+	while (s2[i] != '\0')
+		j++;
+	if (n >= j)
+	{
+		p = (char *) malloc(sizeof(char) * i + j + 1);
+		if (p == NULL)
+			return (NULL);
+		for (k = 0; k < i; k++)
+			p[k] = s1[k];
+		for (k = 0; k < j; k++)
+			p[k + i] = s2[k];
+		p[k + i] = '\0';
+	}
+	else
+	{
+		p = (char *) malloc(sizeof(char) * i + n + 1);
+		if (p == NULL)
+			return (NULL);
+		for (k = 0; k < i; k++)
+			p[k] = s1[k];
+		for (k = 0; k < n; k++)
+			p[k + i] = s2[k];
+		p[k + i] = '\0';
+	}
+	return (p);
+
+
+}
---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,1 @@
+string_nconcat(char *s1, char *s2, unsigned int n);
