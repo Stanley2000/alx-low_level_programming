af15ac47c57064ebffbced39564e63dbe598eb6f
stanley agbo <stanleykelechi2000@gmail.com>
2023-08-11 09:23:41 +0100

    update

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -2,7 +2,7 @@
 /**
   * malloc_checked - a function that allocate a heap memory
   * @b: no of bytes to be allocated
-  * Return: returns a  pointer to the allocated memory 
+  * Return: returns a  pointer to the allocated memory
   */
 
 void *malloc_checked(unsigned int b)
