0bb54ad539b667567fb5d5ce6962225d2d5a5957
stanley agbo <stanleykelechi2000@gmail.com>
2023-08-11 09:16:13 +0100

    update

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,6 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdio.h>
+
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 int *array_range(int min, int max);
