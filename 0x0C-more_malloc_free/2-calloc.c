7da20a43fbc622d90304ef39e604fe362100c81d
stanley agbo <stanleykelechi2000@gmail.com>
2023-08-11 11:13:57 +0100

    update

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+  * _calloc - a function that allocates and set the dynamic memory to zero
+  * @nmemb: the number of elements of the array
+  * @size: the size of the data type
+  * Return: returns a pointer to the memory location
+  */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int i;
+	void *ptr;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	ptr = calloc(nmemb * size);
+
+	if (ptr == NULL)
+		return (NULL);
+	for (i = 0; i < (nmemb * size); i++)
+	{
+		ptr[i] = '0';
+	}
+	return (ptr);
+}
