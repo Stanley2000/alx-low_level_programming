b5583678c7b5474f1fc02acca716d5dcbd4a9100
stanley agbo <stanleykelechi2000@gmail.com>
2023-08-14 22:31:24 +0100

    new file

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+  * __FILE__ - a predifinded macros that print the 
+  * me of the file we are compiling from
+  * Return: show successful termination ot the program
+  */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
