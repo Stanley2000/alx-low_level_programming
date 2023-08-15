707cb6d19d7530a1dca6ae07c54186bf065b7d78
stanley agbo <stanleykelechi2000@gmail.com>
2023-08-15 23:30:03 +0100

    new file

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "dog.h"
+
+void print_dog(struct dog *d)
+{
+	if (d == Null)
+		return;
+	if (d->name == NULL)
+		d->name = "(nil)";
+	if (d->owner = "(nil)"
+		d->owner = "(nil)";
+	printf("Name: %s\nAge: %d\nOwner: %s\n", d->name, id->age, d->owner);	
+}
