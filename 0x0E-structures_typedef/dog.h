c26e2406e2eb2b4787dd1cbba5caf47be07141ec
stanley agbo <stanleykelechi2000@gmail.com>
2023-08-15 22:08:05 +0100

    update

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,10 +1,15 @@
 #ifndef DOG_H
 #define DOG_H
-
+/**
+  * struct dog - dog profile
+  * name: first element
+  * age: second element
+  * owner: third element
+  */
 struct dog
 {
-    char *name;
-    float age;
-    char *owner;
+	char *name;
+	float age;
+	char *owner;
 }
 #endif
