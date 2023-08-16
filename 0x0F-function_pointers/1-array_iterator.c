63e28c39521ccf1db82386e6df1c43a8611e7894
stanley agbo <stanleykelechi2000@gmail.com>
2023-08-16 15:29:39 +0100

    new file

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+  * array_iterator - a function that prints out the array of numbers
+  * @array: array of numbers
+  * @size: the size of the array
+  * @action: pointer to a function that does the printing
+  */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int elem, i;
+
+	if (!array || !action)
+		return;
+
+	for (i = 0; i < size; i++)
+	{
+		elem = array[i];
+		action(elem);
+
+	}
+
+}
