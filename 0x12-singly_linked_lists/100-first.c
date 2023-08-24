e26b2a5056e3f7892ccb44fd3024eb808213d25a
stanley agbo <stanleykelechi2000@gmail.com>
2023-08-25 00:19:27 +0100

    new file

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -3,7 +3,7 @@
 void print_first(void) __attribute__ ((constructor));
 
 /**
- * first - prints a sentence before the main
+ * print_first - prints a sentence before the main
  * function is executed
  */
 void print_first(void)
