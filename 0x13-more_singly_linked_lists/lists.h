8b9f6db1beaa9e61c180964840396caa772e08b8
stanley agbo <stanleykelechi2000@gmail.com>
2023-08-28 22:33:42 +0100

    new file

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,5 +1,5 @@
-#ifndef LIST_H_
-#define LIST_H_
+#ifndef LIST_H
+#define LIST_H
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -19,5 +19,7 @@ typedef struct listint_s
 	int n;
 	struct listint_s *next;
 } listint_t;
+
+size_t print_listint(const listint_t *h);
 
 #endif
