e5285c7302ee2ce97e52a70fbe83e4ed39540760
stanley agbo <stanleykelechi2000@gmail.com>
2023-08-29 20:39:01 +0100

    new file

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -15,7 +15,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *temp = *head;
 
 	new_node = malloc(sizeof(listint_t));
-	if (new_node == NULL)
+	if (!new_node || !head)
 		return (NULL);
 
 	new_node->n = n;
@@ -34,7 +34,12 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		i++;
 	}
 
-	new_node->next = temp->next;
-	temp->next = new_node;
-	return (new_node);
+	if (i == idx - 1)
+	{
+		new_node->next = temp->next;
+		temp->next = new_node;
+		return (new_node);
+	}
+	else
+		return (NULL);
 }
