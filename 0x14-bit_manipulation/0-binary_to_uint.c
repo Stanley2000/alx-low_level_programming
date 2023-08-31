b6de8b1e303cb92495c74ed0b2447dfb07f6daaf
stanley agbo <stanleykelechi2000@gmail.com>
2023-08-31 20:57:56 +0100

    new file

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,40 @@
+iinclude "main.h"
+
+/**
+  * binary_to_uint - a function that converts a binary to decimal equivalent
+  * @b: a pointer to the decimal number
+  * Return: returns the decimal value or null if the function fails
+  */
+
+unsigned int binary_to_uint(const char *b)
+{
+	if (b == NULL)
+		return (0);
+
+	unsigned int result = 0;
+	unsigned int index = 0;
+	int len = 0;
+	int i = 0;
+
+	while (b[len] != '\0')
+	{
+		if (b[len] != '0' || b[len] != '1')
+		{
+			return (0);
+		len++;
+		}
+
+	i = len - 1;
+
+	while (i >= 0)
+	{
+		if (b[i] == '1')
+		{
+			index = (len - i) - 1;
+			result += (1 << index);
+
+		}
+		i--;
+	}
+	return (result);
+}
---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+
+#include <stdio.h>
+#include <stdlib.h>u
+
+unsigned int binary_to_uint(const char *b);
+
+#endif
