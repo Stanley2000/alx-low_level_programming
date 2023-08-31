743ba49ccb1844b0257622df5e5abed22f5c3941
stanley agbo <stanleykelechi2000@gmail.com>
2023-09-01 00:03:00 +0100

    update

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -6,7 +6,7 @@
  */
 void print_binary(unsigned long int n)
 {
-	int i, position = 0;
+	int i, count = 0;
 	unsigned long int current;
 
 	for (i = 63; i >= 0; i--)
@@ -16,11 +16,11 @@ void print_binary(unsigned long int n)
 		if (current & 1)
 		{
 			_putchar('1');
-			position++;
+			count++;
 		}
-		else if (position)
+		else if (count)
 			_putchar('0');
 	}
-	if (!position)
+	if (!count)
 		_putchar('0');
 }
